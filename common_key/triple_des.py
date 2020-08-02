5e0ecd496b6957e4962e5f5475dc820bfc00ab62
P-SiZK <54477535+P-SiZK@users.noreply.github.com>
2020-08-02 19:28:23 +0900

    fix

--- common_key/triple_des.py
+++ common_key/triple_des.py
@@ -9,7 +9,7 @@ import random
 def key_gen(option=3): # option 1:1key(DES), 2:2key, 3:3key
     key1 = des_key_gen()
     if option == 1:
-        return key1
+        return key1,
 
     key2 = des_key_gen()
     if option == 2:
@@ -45,13 +45,12 @@ def main():
     plain_m = input("input m >> ")
     m = str_to_bin(plain_m)
 
-    key1, key2, key3 = key_gen()
-    c = encrypt(m, key1, key2, key3)
-    m_ = decrypt(c, key1, key2, key3)
+    key = key_gen()
+    c = encrypt(m, *key)
+    m_ = decrypt(c, *key)
 
-    print("key1 \t : %#x" %(key1))
-    print("key2 \t : %#x" %(key2))
-    print("key3 \t : %#x" %(key3))
+    for i, k in enumerate(key, 1):
+        print("key%d \t : %#x" %(i, k))
     print("m \t : %#x %s" %(m, bin_to_str(m)))
     print("c \t : %#x %s" %(c, bin_to_str(c)))
     print("m\' \t : %#x %s" %(m_, bin_to_str(m_)))
